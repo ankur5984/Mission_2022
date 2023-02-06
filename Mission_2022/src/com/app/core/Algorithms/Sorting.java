a663c79fc87ee3647fa01efc594cf77227a1597f
ankur5984 <ankur5984@gmail.com>
2023-02-06 16:21:58 +0530

    D_2023-> removed unused code.

--- Mission_2022/src/com/app/core/Algorithms/Sorting.java
+++ Mission_2022/src/com/app/core/Algorithms/Sorting.java
@@ -4,36 +4,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sorting {
-    // write code here 
-//	public static int[] getIntegers(int size){
-//        Scanner sc = new Scanner(System.in);
-//        int[] arr = new int[size];
-//        for(int i=0; i<arr.length;i++){
-//            arr[i] = sc.nextInt();
-//        }
-//        return arr;
-//    }
-//    
-//	public static void printArray(int[] arr){
-//	int j = 0;
-//        for(int i=arr.length-1; i>=0;i--){
-//        	++j;
-//            System.out.println("ELement "+ j + " contents" + arr[i]);
-//        }
-//    }
-//    
-//	public static int[] sortIntegers(int[] arr){
-//        for(int i = 0;i<arr.length;i++){
-//            for(int j=0;j<arr.length-i-1;j++){
-//                if(arr[j]>arr[j+1]){
-//                    int temp = arr[j+1];
-//                    arr[j+1] = arr[j];
-//                    arr[j] = temp;
-//                }
-//            }
-//        }
-//        return arr;
-//    } 
 	
 	public static int[] getIntegers(int size){
         Scanner sc = new Scanner(System.in);
